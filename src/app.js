897e2d204e5b501488836e76e05a7e810e76d00e
David Buser <busdav@outlook.com>
2018-11-04 07:14:51 -0800

    WIP deploy to production

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
 
+const appConfig = require("../src/config/main-config.js");
 const routeConfig = require("../src/config/route-config.js");
+
+appConfig.init();
 routeConfig.init(app);
 
 
