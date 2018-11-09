f2a4d5a5ebfb2a2490564fb1caffa9b61f611aa9
busdav <busdav@outlook.com>
2018-11-08 21:17:31 -0800

    completed routing

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const app = express();
 const appConfig = require("../src/config/main-config.js");
 const routeConfig = require("../src/config/route-config.js");
 
-appConfig.init();
+appConfig.init(app, express);
 routeConfig.init(app);
 
 
