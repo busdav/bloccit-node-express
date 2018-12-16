f39582bf820cf599c15213c447a351452a7e997a
David Buser <busdav@outlook.com>
2018-12-16 20:29:56 +0000

    complete post-resource-2

--- src/db/queries.topics.js
+++ src/db/queries.topics.js
@@ -1,4 +1,5 @@
 const Topic = require("./models").Topic;
+const Post = require("./models").Post;
 
 module.exports = {
 
@@ -14,8 +15,13 @@ module.exports = {
   },
 
   getTopic(id, callback){
-     return Topic.findById(id)
-     .then((topic) => {
+    return Topic.findById(id, {
+      include: [{
+        model: Post,
+        as: "posts"
+      }]
+    })
+    .then((topic) => {
        callback(null, topic);
      })
      .catch((err) => {
